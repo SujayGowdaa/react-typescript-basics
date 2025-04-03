320e944487c497aaa66249b15b90980753ca2b7c
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2025-04-03 14:39:28 +0530

    add delete goal functionality

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,9 +1,11 @@
 import { type FC, type ReactNode } from 'react';
 
 type CourseGoalProps = {
+  id: number;
   title: string;
   description?: string;
   children?: ReactNode;
+  onDelete: (id: number) => void;
 };
 
 // export default function CourseGoal({
@@ -40,14 +42,20 @@ type CourseGoalProps = {
 
 // OR
 
-const CourseGoal: FC<CourseGoalProps> = ({ title, description, children }) => {
+const CourseGoal: FC<CourseGoalProps> = ({
+  id,
+  title,
+  description,
+  children,
+  onDelete,
+}) => {
   return (
     <article>
       <div>
         <h2>{title}</h2>
         <p>{children || description}</p>
       </div>
-      <button>Delete</button>
+      <button onClick={() => onDelete(id)}>Delete</button>
     </article>
   );
 };
--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -3,15 +3,21 @@ import CourseGoal from './CourseGoal';
 
 type GoalsType = {
   goals: GoalType[];
+  onDeleteGoal: (id: number) => void;
 };
 
-export default function CourseGoalList({ goals }: GoalsType) {
+export default function CourseGoalList({ goals, onDeleteGoal }: GoalsType) {
   return (
     <ul>
       {goals.map((goal) => {
         return (
           <li key={goal.id}>
-            <CourseGoal title={goal.title} description={goal.description} />
+            <CourseGoal
+              id={goal.id}
+              title={goal.title}
+              description={goal.description}
+              onDelete={onDeleteGoal}
+            />
           </li>
         );
       })}
