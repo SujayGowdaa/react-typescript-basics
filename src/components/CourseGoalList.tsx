3c2b42d03a01b1878acc8f0ad7d6c745fdb0a6c6
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2025-04-04 17:26:50 +0530

    set props conditionally

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,8 @@
+import { type ReactNode } from 'react';
 import { type GoalType } from '../App';
+
 import CourseGoal from './CourseGoal';
+import InfoBox from './InfoBox';
 
 type GoalsType = {
   goals: GoalType[];
@@ -7,20 +10,36 @@ type GoalsType = {
 };
 
 export default function CourseGoalList({ goals, onDeleteGoal }: GoalsType) {
+  if (goals.length < 1) {
+    return <InfoBox mode='hint'>You have no course goal</InfoBox>;
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode='warning' severity='high'>
+        You're collecting a lot of goals. Don't put too much on the plate!
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((goal) => {
-        return (
-          <li key={goal.id}>
-            <CourseGoal
-              id={goal.id}
-              title={goal.title}
-              description={goal.description}
-              onDelete={onDeleteGoal}
-            />
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => {
+          return (
+            <li key={goal.id}>
+              <CourseGoal
+                id={goal.id}
+                title={goal.title}
+                description={goal.description}
+                onDelete={onDeleteGoal}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
